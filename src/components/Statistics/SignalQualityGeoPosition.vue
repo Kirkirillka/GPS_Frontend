4cb1d02beaafbcc9547fbab0e182b71adeffa836
Kir <kirillkukarcev@gmail.com>
2019-12-15 15:38:09 +0100

    Use plotly library to build a contour plot.

--- src/components/Statistics/SignalQualityGeoPosition.vue
+++ src/components/Statistics/SignalQualityGeoPosition.vue
@@ -1,116 +1,71 @@
+<!--Credits  :http://bl.ocks.org/supereggbert/aff58196188816576af0-->
+
 <template>
-    <div>
-        <svg id="my_dataviz">
-            <g id="scale">
-                <g id="y_axis"></g>
-                <g id="x_axis"></g>
-            </g>
-        </svg>
+    <div id="signal-geo-contour">
     </div>
 </template>
 
 <script>
 
-    import * as d3 from 'd3v4';
+    //import * as d3 from 'd3v4';
+    import * as Plotly from "plotly.js-dist";
 
     export default {
         name: "SignalQualityGeoPosition",
         props: ["series", "window_size", "selected_dates"],
         watch: {
             get_series: function () {
-                // eslint-disable-next-line no-console
-                console.log("updated")
-                this.draw_scatter()
-            }
+                this.draw_contour()
+            },
         },
         mounted: function () {
-            this.draw_scatter()
+            this.draw_contour()
         },
 
         methods: {
-            draw_scatter: function () {
-
-                // set the dimensions and margins of the graph
-                var margin = {top: 10, right: 30, bottom: 40, left: 50},
-                    width = 700 - margin.left - margin.right,
-                    height = 700 - margin.top - margin.bottom;
-
-                // append the svg object to the body of the page
-                var svg = d3.select("#my_dataviz")
-                    .attr("width", width + margin.left + margin.right)
-                    .attr("height", height + margin.top + margin.bottom)
-                    .select("#scale")
-                    .attr("transform",
-                        "translate(" + margin.left + "," + margin.top + ")");
+            draw_contour: function () {
 
 
-                var x_min = d3.min(this.get_series, r => r.x) - 1
-                var x_max = d3.max(this.get_series, r => r.x) + 1
-
-                var y_min = d3.min(this.get_series, r => r.y) - 1
-                var y_max = d3.max(this.get_series, r => r.y) + 1
-
-                //Color interpolation
-                var color_interpolation = d3.interpolateHsl("red", "blue")
+                let x = this.get_series.map(d => d.x).slice(0, this.window_size)
+                let y = this.get_series.map(d => d.y).slice(0, this.window_size)
+                let z = this.get_series.map(d => d.z).slice(0, this.window_size)
 
 
-                // Add X axis
-                var x = d3.scaleLinear()
-                    .domain([x_min, x_max])
-                    .range([0, width])
+                var data = [{
+                    x: x,
+                    y: y,
+                    z: z,
+                    marker: {
+                        size: 3,
+                        opacity: 0.7,
+                    },
+                    type: 'contour',
+                    contours: {
+                        coloring: 'heatmap',
+                        showlabels: true,
+                        labelfont: {
+                            family: 'Raleway',
+                            size: 12,
+                            color: 'white',
+                        }
+                    },
+                    colorbar: {
+                        title: 'RSSI levels',
+                        titleside: 'right',
+                        titlefont: {
+                            size: 14,
+                            family: 'Arial, sans-serif'
+                        }
+                    }
+                }];
 
-                svg.select("#x_axis")
-                    .attr("transform", "translate(0," + height + ")")
-                    .call(d3.axisBottom(x));
-
-
-                // Add Y axis
-                var y = d3.scaleLinear()
-                    .domain([y_min, y_max])
-                    .range([height, 0])
-                    .nice()
-
-                svg.select("#y_axis")
-                    .call(d3.axisLeft(y));
-
-
-                // Update the array of dots
-                var persisted_circles = svg.selectAll("circle")
-                    .data(this.get_series)
-                    .enter()
-                    .append("circle")
-
-                // Find out which dots to delete
-                var circles_to_delete = svg.selectAll("circle")
-                    .data(this.get_series)
-                    .exit()
-
-                // Delete stage
-                circles_to_delete.remove()
-
-
-                // eslint-disable-next-line no-console
-                console.log(persisted_circles)
-                // eslint-disable-next-line no-console
-                console.log(circles_to_delete)
-
-                // Update stage
-                svg.selectAll("circle")
-                    .data(this.get_series)
-                    .attr("cx", function (d) {
-                        return x(d.x);
-                    })
-                    .attr("cy", function (d) {
-                        return y(d.y);
-                    })
-                    .attr("r", "3px")
-                    .attr("fill", function (d) {
-                        return color_interpolation(d.signal)
-                    })
-
-
-
-            }
+                var layout = {
+                    title: 'Signal Quality in the plane',
+                    autosize: true,
+                    height: 1000,
+                };
+                Plotly.newPlot('signal-geo-contour', data, layout, {responsive: true});
+            },
         },
         computed: {
             get_series: function () {
@@ -125,13 +80,14 @@
 
                         return left <= target_date && target_date <= right
                     }).flatMap(function (d) {
-                            return {
-                                'x': parseFloat(d.latitude),
-                                'y': parseFloat(d.longitude),
-                                'signal': d.signal
-                            }
-                        })
+                        return {
+                            'x': parseFloat(d.latitude),
+                            'y': parseFloat(d.longitude),
+                            'z': d.signal
+                        }
+                    })
                 })
+
             }
         }
     }
