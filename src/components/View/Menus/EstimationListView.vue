2360f3972a4566a9a0ea922cd4dd6ddbb7c9ea53
Kir <kirillkukarcev@gmail.com>
2020-01-09 23:03:14 +0100

    Prettify table for estimation, show less information.

--- src/components/View/Menus/EstimationListView.vue
+++ src/components/View/Menus/EstimationListView.vue
@@ -1,9 +1,15 @@
 <template>
     <div>
-        <b-table sticky-header hover :items="uavs_estimated_locations" head-variant="light"
-        v-on:row-clicked="set_current_estimation">
-            List of estimations
-        </b-table>
+        <b-form-row>
+            <b-col>
+                <b-table  small sticky-header :items="uavs_estimated_locations" :fields="fields"
+                              head-variant="light"
+                              v-on:row-clicked="set_current_estimation"
+                              :tbody-transition-props="transProps">
+                    List of estimations
+                </b-table>
+            </b-col>
+        </b-form-row>
     </div>
 </template>
 
@@ -11,8 +17,42 @@
 
     import {mapGetters} from 'vuex';
     import {mapActions} from 'vuex';
+
     export default {
         name: "EstimationListView",
+        data: function () {
+            return {
+                transProps: {
+                    // Transition name
+                    name: 'flip-list'
+                },
+                fields: [
+                    {
+                        label: "Estimation time",
+                        key: "time.value",
+                        sortable: true
+                    },
+                    {
+                        key: "payload.method",
+                        label: "Method"
+                    },
+                    {
+                        label: "UAVs",
+                        key: "payload.suggested",
+                        formatter: (value,) => {
+                            return value.length
+                        }
+                    },
+                    {
+                        label: "UEs",
+                        key: "payload.ues_location",
+                        formatter: (value,) => {
+                            return value.length
+                        }
+                    }
+                ]
+            }
+        },
         computed: {
             ...mapGetters("data", {
                 uavs_estimated_locations: "GET_UAVS_ESTIMATED_LOCATIONS",
